07c8127ab87f7c4165e6aaf33630cc9d92e96681
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-05-29 17:11:00 -0300

    primeira dobra do site institucional - navbar e banner - finalizada

--- Impact/impact-institucional/src/App.js
+++ Impact/impact-institucional/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
-
+import Banner from './components/banner/Banner';
 function App() {
   return (
     <>
+      <Banner />
       <NavBar />
     </>
   );
--- Impact/impact-institucional/src/components/navbar/NavBar.jsx
+++ Impact/impact-institucional/src/components/navbar/NavBar.jsx
@@ -5,21 +5,36 @@ import logo from '../../utils/assets/img/logo.png'
 import { Button } from 'primereact/button';
 
 const items = [
-    { label: 'Home' },
-    { label: 'Serviços' },
-    { label: 'Competências' },
-    { label: 'Sobre' },
-    { label: 'Nosso Time' }
+    {
+        label: 'Home',
+        className: 'ml-8'
+    },
+    {
+        label: 'Serviços',
+        className: 'ml-5'
+    },
+    {
+        label: 'Competências',
+        className: 'ml-5'
+    },
+    {
+        label: 'Sobre',
+        className: 'ml-5'
+    },
+    {
+        label: 'Nosso Time',
+        className: 'ml-5'
+    }
 ];
 
 const logoNav = <img src={logo} alt="logo-impact" className={styles['logo']} />;
-const contato =  <Button label="Contate-nos" className="p-button-rounded" />
+const contato = <Button label="Contate-nos" className="p-button-rounded font-light bg-orange-400 border-none" />
 const NavBar = () => {
-    return(
-    <div className={styles['card']}>
-        <Menubar className={styles['menu']} start={logoNav} model={items} end={contato}>
-        </Menubar>
-    </div>);
+    return (
+        <div className={styles['card']}>
+            <Menubar className={[styles['menu'], 'z-2 absolute']} start={logoNav} model={items} end={contato}>
+            </Menubar>
+        </div>);
 }
 
 export default NavBar;
