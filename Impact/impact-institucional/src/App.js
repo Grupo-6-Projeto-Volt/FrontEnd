bea099486cb85f1a126792970a755290e4453884
Amanda Cupola Alves <125586676+AmandaCupolaa@users.noreply.github.com>
2024-05-30 16:39:48 -0300

    feat: Secção de competencias

--- Impact/impact-institucional/src/App.js
+++ Impact/impact-institucional/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
 import FaleConosco from './components/faleconosco/FaleConosco';
+import Competencias from './components/competencias/Competencias';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Competencias />
       <FaleConosco />
     </>
   );
