ee9d517e7f25552f1e36ebbf6687172afcb37a5d
Amanda Cupola Alves <125586676+AmandaCupolaa@users.noreply.github.com>
2024-05-30 15:38:23 -0300

    feat: Secção de fale conosco

--- Impact/impact-institucional/src/App.js
+++ Impact/impact-institucional/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import FaleConosco from './components/faleconosco/FaleConosco';
 
 function App() {
   return (
     <>
       <NavBar />
+      <FaleConosco />
     </>
   );
 }
