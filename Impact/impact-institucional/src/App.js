e354ce0ea0d6644c05f2b98b631dea960c4413bb
Amanda Cupola Alves <125586676+AmandaCupolaa@users.noreply.github.com>
2024-05-30 16:48:32 -0300

    feat: Secção do footer

--- Impact/impact-institucional/src/App.js
+++ Impact/impact-institucional/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/navbar/NavBar';
 import FaleConosco from './components/faleconosco/FaleConosco';
 import Competencias from './components/competencias/Competencias';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Competencias />
       <FaleConosco />
+      <Footer />
     </>
   );
 }
