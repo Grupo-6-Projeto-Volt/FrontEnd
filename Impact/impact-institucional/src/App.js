1884a751a85ae2e37658ed8d7be8e2052f1d3ad3
AmandaCupolaa <amanda.alves@sptech.school>
2024-06-04 20:20:56 -0300

    feat: seccao sobre nós

--- Impact/impact-institucional/src/App.js
+++ Impact/impact-institucional/src/App.js
@@ -3,12 +3,14 @@ import NavBar from './components/navbar/NavBar';
 import FaleConosco from './components/faleconosco/FaleConosco';
 import Competencias from './components/competencias/Competencias';
 import Footer from './components/footer/Footer';
+import SobreNos from './components/sobrenos/Sobrenos';
 
 function App() {
   return (
     <>
       <NavBar />
       <Competencias />
+      <SobreNos />
       <FaleConosco />
       <Footer />
     </>
