a2c6beb091be0b470348e774b9f9520e6d89da81
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-06-09 21:55:05 -0300

    fix: conflitos de merge ajustados + correções visuais de espaçamentos e nomes de site

--- Cliente/src/App.js
+++ Cliente/src/App.js
@@ -3,16 +3,18 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Chamados from "./pages/dashboard/chamados/Chamados";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Login />} />
 				<Route path="/home" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/cadastro" element={<Cadastro />} />
 				<Route path="/dashboard-chamados" element={<Chamados />}></Route>
+				<Route path="/dashboard" element={<Dashboard />}></Route>
 			</Routes>
 		</>
 	);
