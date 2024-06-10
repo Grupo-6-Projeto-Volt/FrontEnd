c80bc2ca8b2382bbc252f6b1a745a273a1d14734
Davi Hilário <davihilario23@gmail.com>
2024-06-10 18:21:54 -0300

    fix: corrigindo icone de busca

--- Cliente/src/components/searchbar/Searchbar.jsx
+++ Cliente/src/components/searchbar/Searchbar.jsx
@@ -1,24 +1,17 @@
+import { BiSearch } from "react-icons/bi";
 import styles from "./Searchbar.module.css";
 
 function Searchbar({ onChange, onClick, placeholder, width, maxLength }) {
 	return (
 		<div className={styles["Searchbar"]} style={{ width: width }}>
-			<img
-				src={require("../../utils/assets/icon/lupa.png")}
-				alt="Buscar"
-				style={{
-					width: "30px",
-					height: "30px",
-					cursor: "pointer",
-				}}
-				onClick={onClick}
-			/>
+			<BiSearch size={30} cursor={"pointer"} onClick={onClick} />
 			<input
 				type="text"
 				placeholder={placeholder}
 				onChange={(e) => {
 					onChange(e.target.value);
 				}}
+				onBlur={onClick}
 				maxLength={maxLength}
 			/>
 		</div>
