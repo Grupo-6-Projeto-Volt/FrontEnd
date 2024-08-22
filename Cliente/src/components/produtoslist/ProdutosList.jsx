ce674cb7cc0bf0cfd7f9ec6e8c8768d1eb6ccf6e
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-08-22 18:26:18 -0300

    merge

--- Cliente/src/components/produtoslist/ProdutosList.jsx
+++ Cliente/src/components/produtoslist/ProdutosList.jsx
@@ -1,25 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import styles from "./ProdutosList.module.css"
 import { produtos } from "../../model/ProdutosListModel"
 import { Produto } from "../productcard/ProductCard";
 import React, { useEffect, useState } from 'react';
 
-export const list = document.getElementById('item-list');
-export const itemWidth = 150;
+export const itemWidth = 130;
 export const padding = 16;
 export const prev = document.getElementById('prev-btn');
 export const next = document.getElementById('next-btn');
-// import * as ProdutosList from './ProdutosList';
-export function ProdutosData({ secao }) {
+export function ProdutosData({ secao, nome }) {
     let [dadosProduto, setDadosProduto] = useState([]);
 
+    function handleClick(btn) {
+        const list = document.getElementById(`item-list-${nome}`);
+        const itemWidth = 185
+        const padding = 8
+        if (btn === 'prev') {
+            list.scrollLeft -= (itemWidth + padding)
+        } else {
+            list.scrollLeft += (itemWidth + padding)
+        }
+    }
     async function getProdutos() {
         let response;
         try {
-            if (secao === "Ofertas") {
-                response = await produtos.listarOfertas();
-            } else {
-                response = await produtos.listarProdutos()
-            };
+            switch (secao) {
+                case ("Ofertas"):
+                    response = await produtos.listarOfertas();
+                    break;
+                default:
+                    response = await produtos.listarProdutos()
+                    break;
+            }
+
             setDadosProduto(response);
         } catch (e) {
             console.log(e);
@@ -33,46 +46,28 @@ export function ProdutosData({ secao }) {
         getProdutos()
     }, [])
 
-    // function onClick(btn){
-    //     if(btn === 'prev'){
-    useEffect(() => {
-        if(prev){
-            prev.addEventListener('click',()=>{
-                list.scrollLeft -= (itemWidth + padding);
-            })
-        }
-        if(next){
-            next.addEventListener('click',()=>{
-                list.scrollLeft += (itemWidth + padding)
-            })
-        }
-    })
-            
-        // }else{
-        // }
-    // }
-    
-    // return (
-    //     // <div className={styles['listaProdutos']}>
-    //     <div className={styles['container']}>
-    //         <div className={styles['carousel-view']}>
-    //             <button id="prev-btn" class="prev-btn"> </button>
-    //             <div id="item-list" className={styles['item-list']}>
-    //                 {dadosProduto.map((produto) => (
-    //                     <Produto className={styles['item']}
-    //                         nome={produto.nome}
-    //                         estado={produto.estadoGeral}
-    //                         imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrhiivQ-a9g_wMJYALyFjZIE9ylQuwprSM3A&s"
-    //                         preco={produto.preco} />
-    //                 ))}
-    //             </div>
+    dadosProduto.map((produto) => (console.log(produto.imagensProduto.at(0))))
 
-    //             <button id="next-btn" class="next-btn"> </button>
-    //         </div>
-    //     </div> 
-    // )
+    return (
+        // <div className={`${nome}`}>
+            <div className={styles['container']}>
+                <h1>{secao}</h1>
+                <div className={styles['carousel-view']}>
+                    <button id={`prev-btn-${nome}`} onClick={handleClick.bind(this, 'prev')} className={styles['btn-prev']}> </button>
+                    <div id={`item-list-${nome}`} className={styles['item-list']}>
+                        {dadosProduto.map((produto) => (
+                            <Produto className={styles['item']}
+                                nome={produto.nome}
+                                estado={produto.estadoGeral}
+                                imgUrl={produto.imagensProduto.at(0).codigoImagem}
+                                preco={produto.preco} />
+                        ))}
+                    </div>
+                    <button id={`next-btn-${nome}`} onClick={handleClick.bind(this, 'next')} className={styles['btn-next']}> </button>
+                </div>
+            </div>
+        // </div>
+    )
 }
 
 
-
- 
---
+++ Cliente/src/routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Chamados from "./pages/dashboard/chamados/Chamados";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx"
+import ProductPage from "./pages/productpage/ProductPage.jsx";
+
+function Rotas() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/cadastro" element={<Cadastro />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/productpage" element={<ProductPage />} />
+                    <Route path="/dashboard-chamados" element={<Chamados />}></Route>
+                    <Route path="/dashboard" element={<Dashboard />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+export default Rotas;
