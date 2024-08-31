73a208bc786857b53b64bd2ca5b958d49264d43c
AmandaCupolaa <amanda.alves@sptech.school>
2024-08-31 16:50:16 -0300

    Adição do banner

--- Cliente/src/pages/home/Home.jsx
+++ Cliente/src/pages/home/Home.jsx
@@ -7,12 +7,14 @@ import styles from "./Home.module.css";
 import Footer from "../../components/footer/Footer.jsx";
 import NossosServicos from "../../components/nossosServicos/NossosServicos.jsx";
 import Avaliacoes from "../../components/avaliacoes/Avaliacoes.jsx";
+import Banner from "../../components/banner/Banner.jsx";
 
 const Home = () => {
 	return (
 		<>
 			<NavBarPadrao />
 			<div className={styles["container"]}>
+				<Banner></Banner>
 				<Lancamentos className="lancamentos"
 					secao='Lançamentos'
 					nome = 'lancamentos' />
