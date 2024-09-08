517a49986f416bb160f0963db9f4cfe31a416cbd
AmandaCupolaa <amanda.alves@sptech.school>
2024-09-07 22:07:29 -0300

    feat: Sobre da home

--- Cliente/src/components/sobre/Sobre.jsx
+++ Cliente/src/components/sobre/Sobre.jsx
@@ -1,14 +1,15 @@
 import styles from './Sobre.module.css'
 import React from "react";
-import imgWhats from "../../utils/assets/icon/01_Glyph/01_Digital/02_SVG/Green/Digital_Glyph_Green.svg"
+import imgWhats from "../../utils/assets/img/whatsapp.png"
+
 export const Sobre = () => {
     return (
         <div className={styles["container"]}>
             <div className={styles["texto"]}>
                 <h1>Sobre Nós</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
-                <p>Standard dummy text ever since the 1500s, when an unknown printer took a galley of type.</p>
-                <p>Rua do Balacobaco n°3
+                <p>Bem-vindo à nossa loja de eletrônicos! Com anos de experiência no mercado, <br>
+                </br>oferecemos produtos de qualidade e atendimento especializado para ajudá-lo<br>
+                </br> a fazer a melhor escolha. Conte conosco para uma compra segura e eficiente !!!
                     <br />Liberdade</p>
                 <div className={styles['contato-div']}>
                     <button className={styles['contato-btn']}>Entre em contato conosco</button><img className={styles['img-whats']} alt="Imagem Whatsapp" src={imgWhats} />
