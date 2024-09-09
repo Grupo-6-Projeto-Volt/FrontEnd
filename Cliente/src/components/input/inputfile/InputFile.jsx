d6522ae960dbd50540d008f99145af05d31e4159
Davi Hilário <davihilario23@gmail.com>
2024-09-08 21:50:30 -0300

    feat: criando tela para a configuração do banner e das propagandas do site

--- Cliente/src/components/input/inputfile/InputFile.jsx
+++ Cliente/src/components/input/inputfile/InputFile.jsx
@@ -1,16 +1,16 @@
 import styles from "./InputFile.module.css";
 
-function InputFile({ tituloCampo, textoBotao, multiple, onChange }) {
+function InputFile({ id, tituloCampo, textoBotao, multiple, onChange }) {
 	return (
 		<div className={styles["InputFile"]}>
 			<label htmlFor="">{tituloCampo}</label>
-			<label htmlFor="file" className={styles["input-file-button"]}>
+			<label htmlFor={id || "file"} className={styles["input-file-button"]}>
 				{textoBotao}
 			</label>
 			<input
 				type="file"
-				name="file"
-				id="file"
+				name={id || "file"}
+				id={id || "file"}
 				multiple={multiple | false}
 				accept=".png, .jpg, .jpeg"
 				onChange={onChange}
