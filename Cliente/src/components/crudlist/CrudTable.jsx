920a7ca8cc2001ca24cc05df680efa33fd8c9ff4
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-11-25 19:52:25 -0300

    identação

--- Cliente/src/components/crudlist/CrudTable.jsx
+++ Cliente/src/components/crudlist/CrudTable.jsx
@@ -32,6 +32,7 @@ function CrudTable({
 			setSelectedIndex(--selectedIndex);
 		}
 	}
+	
 	function handleNext() {
 		if ((selectedIndex + 1) * limit < values.length) {
 			setSelectedIndex(++selectedIndex);
