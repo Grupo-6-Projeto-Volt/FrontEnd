de15be439ecf7fec851390c30f5001a37854df5f
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-11-25 20:04:50 -0300

    teste

--- Cliente/src/components/crudlist/CrudTable.jsx
+++ Cliente/src/components/crudlist/CrudTable.jsx
@@ -32,7 +32,7 @@ function CrudTable({
 			setSelectedIndex(--selectedIndex);
 		}
 	}
-	//teste meu deus agr vai
+	//teste i
 	function handleNext() {
 		if ((selectedIndex + 1) * limit < values.length) {
 			setSelectedIndex(++selectedIndex);
