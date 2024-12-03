1511fb161cfc119f60567657e9680e13be2194f3
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-12-03 20:58:20 -0300

    Teste

--- Cliente/src/setupProxy.js
+++ Cliente/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/java',
         createProxyMiddleware({
-            target: "https://10.0.0.155:8080",
+            target: "https://10.0.0.155:8080", 
             changeOrigin: false,
             pathRewrite: {
                 '^/java': ''
