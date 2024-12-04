62474d009aa60d6f6a57aea4cd4a23ba39fd79be
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-12-03 22:33:44 -0300

    teste

--- Cliente/src/setupProxy.js
+++ Cliente/src/setupProxy.js
@@ -1,14 +1,14 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-        '/java',
-        createProxyMiddleware({
-            target: process.env.REACT_APP_API_URL,
-            changeOrigin: false,
-            pathRewrite: {
-                '^/java': ''
-            }
-        })
-    );
-};
+// module.exports = function(app) {
+//     app.use(
+//         '/java',
+//         createProxyMiddleware({
+//             target: process.env.REACT_APP_API_URL,
+//             changeOrigin: false,
+//             pathRewrite: {
+//                 '^/java': ''
+//             }
+//         })
+//     );
+// };
