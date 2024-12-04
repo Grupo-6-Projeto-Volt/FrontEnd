126444a7bdfa598dc5c29f0141c196349a258f5a
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-12-03 23:37:05 -0300

    teste mudança no deploy

--- Cliente/src/api.js
+++ Cliente/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
-const api = axios.create({baseURL: "107.23.120.117"});
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
+// const api = axios.create({baseURL: "107.23.120.117"});
 
 export default api;
 
